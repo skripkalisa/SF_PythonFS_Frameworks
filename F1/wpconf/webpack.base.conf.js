dae99e45020cf59dedc4ae35b014a791071ca065
skripkalisa <skripkalisa.msk@ya.ru>
2022-01-10 14:16:06 +0300

    midway changes

---
+++ F1/wpconf/webpack.base.conf.js
@@ -0,0 +1,93 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const StylelintPlugin = require('stylelint-webpack-plugin')
+
+const path = require('path')
+
+const PATHS = {
+  src: path.join(__dirname, '../src/'),
+  dist: path.join(__dirname, '../dist/'),
+  build: path.join(__dirname, './build/'),
+  assets: 'assets/',
+}
+const PAGES_DIR = `${PATHS.src}/pug/`
+
+module.exports = {
+  externals: {
+    paths: PATHS,
+  },
+  entry: {
+    app: `${PATHS.src}index.js`,
+  },
+  output: {
+    filename: 'js/[name].[chunkhash].js',
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[chunkhash].css',
+    }),
+    new HtmlWebpackPlugin({ template: `${PAGES_DIR}index.pug` }),
+    new TerserWebpackPlugin(),
+    new OptimizeCSSAssetsPlugin(),
+    new StylelintPlugin(),
+  ],
+  optimization: {
+    minimizer: [new TerserWebpackPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+          // 'ts-loader',eader 1
+        ],
+      },
+
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: true,
+            },
+          },
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.styl$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              esModule: true,
+            },
+          },
+          {
+            loader: 'stylus-loader',
+            options: { sourceMap: true },
+          },
+        ],
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+        options: {
+          pretty: true,
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+}
