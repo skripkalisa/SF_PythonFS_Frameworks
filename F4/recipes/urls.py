7c59938f856f8eb188906eb0696c397d9d764cd9
skripkalisa <skripkalisa.msk@ya.ru>
2022-02-15 02:03:29 +0300

    url paths fixed

--- F4/recipes/urls.py
+++ F4/recipes/urls.py
@@ -5,4 +5,6 @@ from . views import RecipeList
 
 urlpatterns = [
   path('', RecipeList.as_view()),
+  path('recipe/<slug:slug>/', RecipeList.as_view()),
+  path('category/<slug:slug>/', RecipeList.as_view()),
 ]
